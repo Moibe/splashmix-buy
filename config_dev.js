a613cea5d46aa79c3cbed1f3a73586bd00e38cc3
Moibe <moi.estrello@gmail.com>
2025-08-11 00:29:26 -0600

    loader y configs no son necesarios

--- config_dev.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDGYZJ87hn-RfveqC_BydFQEJTMSvS7eK4",
-  authDomain: "splashmix-88e15.firebaseapp.com",
-  projectId: "splashmix-88e15",
-  storageBucket: "splashmix-88e15.firebasestorage.app",
-  messagingSenderId: "429463695136",
-  appId: "1:429463695136:web:5843b892e8761560f77145",
-  measurementId: "G-PHPJCWFF9L"
-};
