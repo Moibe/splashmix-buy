d72c959e9e581be558cd600276d9cb62dcb1a2a6
Moibe <moi.estrello@gmail.com>
2025-09-25 18:43:24 -0600

    Init test prices prod

--- precios.js
+++ precios.js
@@ -14,6 +14,6 @@ export const precios_prod = [
     {id:2, nombre:"🃏40 imágenes", precio:"$580 mxn", cxt:"($14.5/imagen)",  mode: "payment", price_id: "price_1RtdeLIYi36CbmfWXv0WCkNU", imagenes: 40},
     {id:3, nombre:"🃏80 imágenes", precio:"$780 mxn", cxt:"($9.75)/imagen",  mode: "payment", price_id: "price_1RqqQtIYi36CbmfWUyyUs48h", imagenes: 80},
     {id:4, nombre:"🃏320 imágenes", precio:"$1600 mxn", cxt:"($5/imagen)",  mode: "payment", price_id: "price_1RqqKQIYi36CbmfWVlDxTJk1", imagenes: 320},
-    {id:5, nombre:"🃏1000 imágenes", precio:"$1900 mxn", cxt:"($1.9/imagen)",  mode: "payment", price_id: "price_1RqpziIYi36CbmfWJuLkU9Sa", imagenes: 1000},
+    {id:5, nombre:"🃏1000 imágenes", precio:"$1900 mxn", cxt:"($1.9/imagen)",  mode: "payment", price_id: "price_1SBPjIIYi36CbmfWOkNXYLcl", imagenes: 1000},
 ];
 
